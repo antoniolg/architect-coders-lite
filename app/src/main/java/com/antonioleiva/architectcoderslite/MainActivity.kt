de5db315147e9f22510491e95d05a0de40665897
Antonio Leiva <antonio@devexperto.com>
2021-12-28 10:27:30 +0100

    Create Login UI and bind Button click

--- app/src/main/java/com/antonioleiva/architectcoderslite/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/architectcoderslite/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.antonioleiva.architectcoderslite
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.antonioleiva.architectcoderslite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            Toast.makeText(
+                this,
+                "user:${binding.user.text}, pass: ${binding.pass.text}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
