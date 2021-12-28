2bfb25e91b190d61efdf3bb1a9cb9f38b8650cd2
Antonio Leiva <antonio@devexperto.com>
2021-12-29 00:24:18 +0100

    Tests for LoginRemoteDataSource

---
+++ app/src/test/java/com/antonioleiva/architectcoderslite/data/LoginRemoteDataSourceTest.kt
@@ -0,0 +1,35 @@
+package com.antonioleiva.architectcoderslite.data
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class LoginRemoteDataSourceTest {
+
+    private val ds = LoginRemoteDataSource()
+
+    @Test
+    fun `user without @ returns error`(): Unit = runBlocking {
+        val result = ds.tryLogin("user", "")
+        assertTrue(result.userError)
+    }
+
+    @Test
+    fun `user with @ returns success`(): Unit = runBlocking {
+        val result = ds.tryLogin("user@", "")
+        assertFalse(result.userError)
+    }
+
+    @Test
+    fun `pass with less than 5 characters returns error`(): Unit = runBlocking {
+        val result = ds.tryLogin("", "1234")
+        assertTrue(result.passError)
+    }
+
+    @Test
+    fun `pass with 5 characters returns success`(): Unit = runBlocking {
+        val result = ds.tryLogin("", "12345")
+        assertFalse(result.passError)
+    }
+}
